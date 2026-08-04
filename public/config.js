19e4cd1c2df75e91634b96632abc246be83a6b26
reeend1st-max <reeend1st@gmail.com>
2026-08-04 21:49:34 +0700

    Release V108: Complete single atomic update with Belanja Bulanan & Bank Categories

---
+++ public/config.js
@@ -0,0 +1,9 @@
+﻿// Auto-generated runtime configuration
+window.ENV = {
+  SUPABASE_URL: "https://nwrhhclyjuhrmcazigrr.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53cmhoY2x5anVocm1jYXppZ3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzNTA1MTgsImV4cCI6MjA1NTkyNjUxOH0.1NUpJzT2eA47_cndvAStJjYt9p48i9x2KIn1dDqg80Q"
+};
+window.__SUPABASE_CONFIG__ = {
+  url: "https://nwrhhclyjuhrmcazigrr.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im53cmhoY2x5anVocm1jYXppZ3JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAzNTA1MTgsImV4cCI6MjA1NTkyNjUxOH0.1NUpJzT2eA47_cndvAStJjYt9p48i9x2KIn1dDqg80Q"
+};
